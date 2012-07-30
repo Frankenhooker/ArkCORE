494f0805edfe4c24badea2c2561c12255f170646
Darkan <winters@ist-einmalig.de>
2012-07-30 15:59:58 +0200

    DB/item_template: Fixed Hunter Pets feeding with Merchant food.

---
+++ sql/updates/world/12_07_30_03_item_template.sql
@@ -0,0 +1,53 @@
+/* 
+This fixes the Problem of Hunters who cannot feed pets
+with Food bought from Merchants.
+This only covers up cataclysm and WotLK Food Items! 
+*/
+
+UPDATE item_template SET FoodType='1' WHERE entry='58269';
+UPDATE item_template SET FoodType='1' WHERE entry='58267';
+UPDATE item_template SET FoodType='6' WHERE entry='58265';
+UPDATE item_template SET FoodType='2' WHERE entry='58263';
+UPDATE item_template SET FoodType='3' WHERE entry='58259';
+UPDATE item_template SET FoodType='1' WHERE entry='62664';
+UPDATE item_template SET FoodType='1' WHERE entry='62662';
+UPDATE item_template SET FoodType='2' WHERE entry='62661';
+UPDATE item_template SET FoodType='1' WHERE entry='62665';
+UPDATE item_template SET FoodType='2' WHERE entry='62666';
+UPDATE item_template SET FoodType='2' WHERE entry='62677';
+UPDATE item_template SET FoodType='2' WHERE entry='62671';
+UPDATE item_template SET FoodType='1' WHERE entry='62670';
+UPDATE item_template SET FoodType='2' WHERE entry='62669';
+UPDATE item_template SET FoodType='2' WHERE entry='62668';
+UPDATE item_template SET FoodType='2' WHERE entry='62667';
+UPDATE item_template SET FoodType='1' WHERE entry='62676';
+UPDATE item_template SET FoodType='1' WHERE entry='62658';
+UPDATE item_template SET FoodType='1' WHERE entry='62657';
+UPDATE item_template SET FoodType='2' WHERE entry='62654';
+UPDATE item_template SET FoodType='2' WHERE entry='62652';
+UPDATE item_template SET FoodType='1' WHERE entry='62651';
+UPDATE item_template SET FoodType='3' WHERE entry='58258';
+UPDATE item_template SET FoodType='4' WHERE entry='58260';
+UPDATE item_template SET FoodType='2' WHERE entry='58262';
+UPDATE item_template SET FoodType='1' WHERE entry='59232';
+UPDATE item_template SET FoodType='5' WHERE entry='59228';
+UPDATE item_template SET FoodType='1' WHERE entry='58268';
+UPDATE item_template SET FoodType='5' WHERE entry='58266';
+UPDATE item_template SET FoodType='6' WHERE entry='58264';
+UPDATE item_template SET FoodType='1' WHERE entry='41729';
+UPDATE item_template SET FoodType='1' WHERE entry='40202';
+UPDATE item_template SET FoodType='1' WHERE entry='35953';
+UPDATE item_template SET FoodType='3' WHERE entry='35952';
+UPDATE item_template SET FoodType='2' WHERE entry='35951';
+UPDATE item_template SET FoodType='4' WHERE entry='35950';
+UPDATE item_template SET FoodType='6' WHERE entry='35948';
+UPDATE item_template SET FoodType='6' WHERE entry='35947';
+UPDATE item_template SET FoodType='1' WHERE entry='44940';
+UPDATE item_template SET FoodType='3' WHERE entry='44607';
+UPDATE item_template SET FoodType='1' WHERE entry='44072';
+UPDATE item_template SET FoodType='2' WHERE entry='44071';
+UPDATE item_template SET FoodType='6' WHERE entry='43087';
+UPDATE item_template SET FoodType='2' WHERE entry='44049';
+UPDATE item_template SET FoodType='1' WHERE entry='44953';
+UPDATE item_template SET FoodType='1' WHERE entry='43005';
+UPDATE item_template SET FoodType='2' WHERE entry='34762';
